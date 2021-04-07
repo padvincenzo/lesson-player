17d6abc3ae3a1b9592710de4466599aea7010cf2
Vincenzo Padula <32195269+padvincenzo@users.noreply.github.com>
2021-04-07 18:42:54 +0200

    Restyling

--- index.php
+++ index.php
@@ -16,7 +16,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
+include("_language.php");
 ?>
+
 <!DOCTYPE html>
 <html lang="it">
 	<head>
@@ -25,33 +28,33 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- VideoJS -->
-		<script type="text/javascript">window.HELP_IMPROVE_VIDEOJS = false;</script>
-		<script src="https://unpkg.com/video.js@7/dist/video.min.js"></script>
-		<link href="https://unpkg.com/video.js@7/dist/video-js.min.css" rel="stylesheet">
+		<!-- <script type="text/javascript">window.HELP_IMPROVE_VIDEOJS = false;</script> -->
+		<!-- <script src="https://unpkg.com/video.js@7/dist/video.min.js"></script> -->
+		<!-- <link href="https://unpkg.com/video.js@7/dist/video-js.min.css" rel="stylesheet"> -->
+		<script src="video.js@7.11.8/video.min.js"></script>
+		<link href="video.js@7.11.8/video-js.min.css" rel="stylesheet">
 
 		<link href="theme.css" rel="stylesheet">
 		<link href="style.css" rel="stylesheet">
 
 		<script type="text/javascript">
-			<?php
-			include("_language.php");
-			echo "const lang = " . json_encode($lang) . ";";
-			?>
+			<?php echo "const lang = " . json_encode($lang) . ";"; ?>
 		</script>
 
 		<script src="code.js" type="text/javascript"></script>
+		<script src="js/ui.js" type="text/javascript"></script>
 		<script src="js/message.js" type="text/javascript"></script>
 		<script src="js/form.js" type="text/javascript"></script>
 		<script src="js/lesson.js" type="text/javascript"></script>
 		<script src="js/class.js" type="text/javascript"></script>
 		<script src="js/player.js" type="text/javascript"></script>
 
-		<link rel="icon" href="icon.svg">
+		<link rel="icon" href="img/icon.svg">
 	</head>
 
 	<body>
 
-		<div id="my-p-background">
+		<div id="my-p-background" tabindex="1">
 			<div id="my-p-wrapper">
 				<video id="my-player"
 					class="video-js vjs-theme-lesson"
