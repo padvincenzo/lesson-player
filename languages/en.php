e14ca84c3d4ba4d91ab665d4506d93f78de9e283
Vincenzo Padula <32195269+padvincenzo@users.noreply.github.com>
2021-04-23 17:17:35 +0200

    Add files via upload

---
+++ languages/en.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Lesson Player
+View your lessons, do not miss the mark.
+
+Copyright (C) 2021  Vincenzo Padula
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+$langCode = "en";
+$lang = array(
+  "cancel"              => "Cancel",
+  "class"               => "Class",
+  "classAdded"          => "Class added",
+  "classCompleted"      => "This class seems completed",
+  "classDirectory"      => "Class directory",
+  "classEdited"         => "Class edited",
+  "classList"           => "Class list",
+  "className"           => "Class name",
+  "classNotAdded"       => "Class not added",
+  "classNotEdited"      => "Class not edited",
+  "confirm"             => "Confirm",
+  "credits"             => "Credits",
+  "dataMiss"            => "Missing data",
+  "dated"               => "Dated",
+  "dbConnectionFailed"  => "Database connection failed.",
+  "defaultLessonTitle"  => "Lesson of {dated}",
+  "edit"                => "Edit",
+  "errorOpeningFile"    => "Error opening file",
+  "failed"              => "Failed",
+  "ffmpegCopyPaste"     => "Copy and paste output of",
+  "ffmpegOutput"        => "Output of ffmpeg",
+  "filename"            => "File name",
+  "homePage"            => "Home page",
+  "idclass"             => "Class ID",
+  "idlesson"            => "Lesson ID",
+  "installSuccessful"   => "Install successful.",
+  "installFailed"       => "Install failed.",
+  "invalidData"         => "Data not valid",
+  "lessonAdded"         => "Lesson added",
+  "lessonEdited"        => "Lesson edited",
+  "lessonList"          => "Lesson list",
+  "lessonNotAdded"      => "Lesson not added",
+  "lessonNotEdited"     => "Lesson not edited",
+  "nextLesson"          => "Next lesson",
+  "newClass"            => "Add class",
+  "newLesson"           => "Add lesson",
+  "ok"                  => "Ok",
+  "paused"              => "Paused",
+  "play"                => "Play",
+  "playing"             => "Playing",
+  "previousLesson"      => "Previous lesson",
+  "professor"           => "Professor",
+  "progress"            => "Progress",
+  "rate"                => "Rate",
+  "resume"              => "Resume",
+  "show"                => "Show",
+  "silencesList"        => "List of silences",
+  "soundOff"            => "Muted",
+  "soundOn"             => "Unmuted",
+  "success"             => "Success",
+  "started"             => "Started",
+  "title"               => "Title",
+  "toBeWatched"         => "To be watched",
+  "volume"              => "Volume",
+  "watched"             => "Watched"
+);
+
+?>
