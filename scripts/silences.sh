44677490a0f0cd84d5063b5f92ea45bb27b4883a
Vincenzo Padula <32195269+padvincenzo@users.noreply.github.com>
2021-05-03 19:06:56 +0200

    Added a simple filter for the output of ffmpeg

--- scripts/silences.sh
+++ scripts/silences.sh
@@ -4,15 +4,14 @@
 # 2. Open the terminal;
 # 3. Run ./silences.sh
 #
-# This script will save the output produced by ffmpeg in files with the same name of your videos.
+# This script will save the output produced by ffmpeg in text files with the same name of your videos.
 
 for lesson in * ; do
 	if [[ $lesson =~ \.mp4$ ]]; then
-		echo -n "Detecting silences of ${lesson} ... "
-		name=`echo $lesson | cut -d '.' -f 1`
-		txt=${name}.txt
-		ffmpeg -hide_banner -nostats -vn -i "${lesson}" -af silencedetect=n=0.002:d=2.3 -f null - &> "${txt}"
-		echo "done"
+		echo -n "Detecting silences of ${lesson} ..."
+		txt=${lesson}.txt
+		ffmpeg -hide_banner -nostats -vn -i "${lesson}" -af silencedetect=n=0.002:d=2.3 -f null - 2>&1 | grep -E 'silence_(start|end)' > "${txt}"
+		echo " done"
 	fi
 done
 
