a803074f91843f79a895173c21d601978750fb4e
Vincenzo Padula <32195269+padvincenzo@users.noreply.github.com>
2021-05-20 21:49:35 +0200

    Update silences.sh

--- scripts/silences.sh
+++ scripts/silences.sh
@@ -7,10 +7,7 @@
 # This script will save the output produced by ffmpeg in text files with the same name of your videos.
 
 for lesson in *.mp4 ; do
-	echo -n "Detecting silences of ${lesson} ..."
-	txt=${lesson}.txt
-	ffmpeg -hide_banner -nostats -vn -i "${lesson}" -af silencedetect=n=0.002:d=2.3 -f null - 2>&1 | grep -E 'silence_(start|end)' > "${txt}"
-	echo " done"
+	echo -n "Detecting silences of ${lesson} ... "
+	ffmpeg -hide_banner -nostats -vn -i "${lesson}" -af silencedetect=n=0.002:d=2.3 -f null - 2>&1 | grep -E 'silence_(start|end)' > "${lesson}.txt"
+	echo "done"
 done
-
-
