aab9c77d4c9ba81b10a50ad915bfbd0b2f7d0dd7
Vincenzo Padula <32195269+padvincenzo@users.noreply.github.com>
2021-05-24 17:58:06 +0200

    Updated style directory

--- install.php
+++ install.php
@@ -37,7 +37,7 @@ $dbh->close();
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<link href="style.css" rel="stylesheet">
+		<link href="style/style.css" rel="stylesheet">
     <link rel="icon" href="img/icon.svg">
 
     <?php if($result) echo "<meta http-equiv='refresh' content='5; url=./'>"; ?>
