fad3d8740feab913fa8f2578c4c806b58220cfd7
Vincenzo Padula <padvincenzo@gmail.com>
2021-09-11 21:09:10 +0200

    Added shortcuts

--- index.php
+++ index.php
@@ -101,6 +101,8 @@ $dbh->close();
 				<a href="https://vincenzopadula.altervista.org/docs/lesson-player/" target="_blank"><?php echo $lang->documentation; ?></a>
 				&middot;
 				<a href="JavaScript:UI.settings();"><?php echo $lang->settings; ?></a>
+				&middot;
+				<a href="JavaScript:UI.showKeyboardShortcuts();"><?php echo $lang->shortcuts; ?></a>
 			</p>
 			<p id="ip-address"></p>
 		</footer>
